041945a176a98732604ff154df033474ce88ca81
user01 <user01@test.jp>
2022-03-09 14:39:27 +0000

    買付日と売却日を追加

--- app/models/jyoto_eki_meisai.rb
+++ app/models/jyoto_eki_meisai.rb
@@ -2,15 +2,13 @@ class JyotoEkiMeisai < ApplicationRecord
 
   # 譲渡益明細ＤＢから銘柄毎の損益を算出する
   def self.get_soneki(nen)
-    p "aaaaaaaaaa nen = " , nen
     # 日付の組み立て
     from_date = nen + "-01-01"
     to_date = nen + "-12-31"
-p "from_date = " , from_date
-p "to_date = " , to_date
 
     # SQL
-    query = "SELECT code, MAX(name) as name, SUM(son_eki_gaku) as son_eki_gaku " 
+    query = "SELECT code, MAX(name) as name, SUM(son_eki_gaku) as son_eki_gaku, " 
+    query += "MIN(yakujo_bi) AS kaituke_bi, MAX(yakujo_bi) AS baikyaku_bi "
     query += "FROM jyoto_eki_meisais "
     query += "WHERE ukewatasi BETWEEN ? and ?"
     query += "GROUP BY code "
