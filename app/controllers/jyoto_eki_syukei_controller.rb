6d5b11f93c16ecea27525a775a3884217e1cd596
u93b124 <u93b124@gmail.com>
2024-11-08 11:32:59 +0000

    feat: CSVダウンロード機能を追加

--- app/controllers/jyoto_eki_syukei_controller.rb
+++ app/controllers/jyoto_eki_syukei_controller.rb
@@ -21,7 +21,7 @@ class JyotoEkiSyukeiController < ApplicationController
       uri_tanka = (jyoto_eki.konyu_gaku + jyoto_eki.son_eki_gaku) / jyoto_eki.kabu_su
       tou_raku_ritu = ( (uri_tanka.to_f / kai_tanka.to_f) -1 ) * 100
 
-      if ( jyoto_eki.son_eki_gaku < 0 ) 
+      if ( jyoto_eki.son_eki_gaku < 0 )
         @sum_minus = @sum_minus + jyoto_eki.son_eki_gaku   # 合計損失額
         @count_minus = @count_minus + 1                    # 負け数
         sum_tou_raku_ritu_minus = sum_tou_raku_ritu_minus + tou_raku_ritu # 平均損失計算用_合計
@@ -33,10 +33,48 @@ class JyotoEkiSyukeiController < ApplicationController
 
     end
 
-    @avg_loss = sum_tou_raku_ritu_minus.to_f / @count_minus 
+    @avg_loss = sum_tou_raku_ritu_minus.to_f / @count_minus
     @avg_profit = sum_tou_raku_ritu_plus.to_f / @count_plus
     @win_rate = (@count_plus.to_f / (@count_plus.to_f + @count_minus.to_f))*100
 
+    respond_to do |format|
+      format.html
+      format.csv do |csv|
+        send_csv(@jyoto_eki_syukeis)
+      end
+    end
   end
+
+  # CSVダウンロード
+  def send_csv(jyoto_eki_syukeis)
+    bom = %w(EF BB BF).map { |e| e.hex.chr }.join
+    csv_data = CSV.generate(bom) do |csv|
+      # header = %w(code name kabu_su)
+      header = %w(コード 会社名	株数	買付株価	売却単価	騰落率	損益額	買付日	売却日)
+      csv << header
+
+      jyoto_eki_syukeis.each do |item|
+        kai_tanka = item.konyu_gaku / item.kabu_su
+        uri_tanka = (item.konyu_gaku + item.son_eki_gaku) / item.kabu_su
+        tou_raku_ritu = ( (uri_tanka.to_f / kai_tanka.to_f) -1 ) * 100
+
+        values = [
+          item.code,
+          item.name,
+          item.kabu_su,
+          kai_tanka,
+          uri_tanka,
+          tou_raku_ritu,
+          item.son_eki_gaku,
+          item.kaituke_bi,
+          item.baikyaku_bi
+        ]
+        csv << values
+      end
+
+    end
+    send_data(csv_data, filename: "jyoto_eki_syukeis_" + @nen + ".csv")
+  end
+
 
 end
